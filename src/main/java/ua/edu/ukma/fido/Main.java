f8be86318f088c7ce51205e4c071a31b6961a112
TRO <t.babich@ukma.edu.ua>
2020-05-15 16:44:44 +0300

    Project done!

--- src/main/java/ua/edu/ukma/fido/Main.java
+++ src/main/java/ua/edu/ukma/fido/Main.java
@@ -1,8 +1,22 @@
 package ua.edu.ukma.fido;
 
+import ua.edu.ukma.fido.entity.Message;
+import ua.edu.ukma.fido.entity.Packet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Message testMessage = new Message(3, 4, "test");
+        Packet packet = new Packet((byte) 1, 2L, testMessage);
+        System.out.println("Out packet: ");
+        System.out.println(packet);
+        byte[] encodedPacket = packet.toPacket();
+        try {
+            Packet decodedPacket = new Packet(encodedPacket);
+            System.out.println("Int packet: ");
+            System.out.println(decodedPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
